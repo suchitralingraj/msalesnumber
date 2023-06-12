bb50c0601368ef473fee1e433297ab65b16d801f
vadlamudi-n <134616778+vadlamudi-n@users.noreply.github.com>
2023-06-12 11:36:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import {useState} from   'react';
+
+import {StyleSheet,ImageBackground,SafeAreaView} from 'react-native'
+import  LinearGradient  from 'react-native-linear-gradient'
+import StartGamescreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen';
+import Color from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
+
+const App = () => {
+ 
+  const [ userNumber, setUserNumber]=useState();
+  const[gameIsOver,setgameIsOver]=useState(true);
+  const[guessRounds,setGuessRounds]=useState(0);
+
+
+ 
+
+
+  function pickedNumberHandler(pickedNumbr){
+    setUserNumber(pickedNumbr);
+    setgameIsOver(false);
+  }
+  function gameOverhandler( numnerOfRounds){
+    setgameIsOver(true);
+    setGuessRounds(numnerOfRounds);
+  }
+  function startNewGameHandler(){
+    setUserNumber(null);
+    setGuessRounds(0);
+
+  }
+
+
+  let screen =<StartGamescreen onPickNumber={pickedNumberHandler}/>;
+
+  if(userNumber){
+    screen =<GameScreen  userNumber={userNumber} onGameOver={gameOverhandler}/>
+  }
+
+  if(gameIsOver && userNumber){
+    screen=<GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
+
+  }
+  
+
+  return (
+    <LinearGradient  colors= {[Color.primary700,Color.primary800]} style={styles.rootapp}>
+      <ImageBackground  
+      source={require('../numbergame/assets/images/background.png')} 
+      resizeMode='cover'
+      style={styles.rootapp}
+      imageStyle={styles.backgroundimage}
+      >
+        <SafeAreaView style={styles.rootapp}>{screen}</SafeAreaView>
+       </ImageBackground>
+
+    </LinearGradient>
+    
+  )
+}
+
+export default App;
+
+const styles=StyleSheet.create({
+ rootapp:{
+   flex:1,
+ } ,
+
+ backgroundimage:{
+   opacity:0.9,
+ }
+})
